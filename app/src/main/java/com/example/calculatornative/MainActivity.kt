8192d56ab145d4a4e19df89a97866b8932dca7f1
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-01-04 19:46:17 +0600

    minus operation done

--- app/src/main/java/com/example/calculatornative/MainActivity.kt
+++ app/src/main/java/com/example/calculatornative/MainActivity.kt
@@ -48,6 +48,37 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun onEqual(view: View){
+        if(lastNumeric){
+            var tvValue = tvInput?.text.toString()
+            var prefix = ""
+            try {
+                if(tvValue.startsWith("-")){
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+
+                if(tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+
+                    tvInput?.text = (one.toDouble() - two.toDouble()).toString()
+                }
+
+
+
+            } catch (e: ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+    }
+
     private fun isOperatorAdded(value: String): Boolean {
         return if(value.startsWith("-")){
             false
